b3012684b6684f9419c26f998daad61c532443ac
drknozan <drknozan@gmail.com>
2024-02-27 22:16:49 +0300

    feat: setup database connection

---
+++ src/config/db.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+
+export default db;
